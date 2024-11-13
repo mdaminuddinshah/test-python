e6ec26bc8c08a56cd7ef0dcdd7ef07700f658fa8
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-11-13 14:02:43 +0800

    try.py

---
+++ try.py
@@ -0,0 +1,15 @@
+goal = 'good'
+GOAL = 'bad'
+old = 12
+
+# f-string
+print(f"it can be {goal} or {GOAL}, thats {old}")
+
+# .format()
+print("well sometimes it {} or {}, SO JUST BE {}" .format(goal, GOAL, old))
+
+# concatenate
+print("maybe " + goal + " maybe " + GOAL)
+
+# comma
+print("they may be ", goal, " also maybe", GOAL)
